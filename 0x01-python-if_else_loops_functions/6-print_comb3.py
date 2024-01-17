e72a5cc03c9538f31c6b221c64c1f0336e3242dc
Sheelah <sheelah.mogaka@gmail.com>
2024-01-16 17:24:28 -0800

    6-print_comb3.py.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for dig1 in range(10):
     for dig2 in range(dig1 + 1, 10):
         if dig1 < dig2:
-            if digit1 == 8:
+            if dig1 == 8:
                 print("{:d}{:d}".format(dig1, dig2))
             else:
                 print("{:d}{:d}".format(dig1, dig2), end=", ")
