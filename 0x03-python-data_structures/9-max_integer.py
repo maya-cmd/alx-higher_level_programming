bb511dda2ef3995b3530fcfeaa4e3a81754c6254
Sheelah <sheelah.mogaka@gmail.com>
2024-01-23 16:58:44 -0800

    9-max_integer.py.

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return "None"
-    else:
-        max = my_list[0]
-        for i in range(len(my_list)):
-
-            if my_list[i] > max:
-                max == my_list[i]
-        return max
+    if len(my_list) < 0:
+        return None
+    my_list_copy = my_list.copy()
+    my_list_copy.sort()
+    return my_list_copy[-1]
