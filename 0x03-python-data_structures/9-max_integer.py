eaa620ad28c98a1a46a8c9005cdd3b3bd509943d
Sheelah <sheelah.mogaka@gmail.com>
2024-01-23 17:02:43 -0800

    9-max_integer.

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if len(my_list) < 0:
+    if len(my_list) < 1:
         return None
     my_list_copy = my_list.copy()
     my_list_copy.sort()
