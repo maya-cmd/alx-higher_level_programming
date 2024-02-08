eedd6d98ad9f752dbe0e93bc07d45ad1429f3008
Sheelah <sheelah.mogaka@gmail.com>
2024-02-07 17:26:15 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Defines a square"""
+
+
+class Square:
+
+    """Defining a square class"""
+    def __init__(self, size):
+        """
+        Initializing a square that is new
+        Args:
+            size: length of one side
+        """
+
+        self.__size = size
