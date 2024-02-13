debfbb0013dfd6aa448b8f67f2eea4e030a84a55
Sheelah <sheelah.mogaka@gmail.com>
2024-02-12 16:48:58 -0800

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+An empty Rectangle Module
+
+"""
+
+
+class Rectangle:
+    """Defines  a Rectangle"""
+
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is a directory that comprises of more tasks on Python classes.
