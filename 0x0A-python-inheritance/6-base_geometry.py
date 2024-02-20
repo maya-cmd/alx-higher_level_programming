17904fdae8ec5712969f7e0ccf94e6563b25a9b4
Sheelah <sheelah.mogaka@gmail.com>
2024-02-19 17:54:07 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """Method finding the area"""
+        raise Exception("area() is not implemented")
