b3ca2c95eb3332f09d19f9761e213c640b68fe79
Sheelah <sheelah.mogaka@gmail.com>
2024-02-21 03:47:44 -0800

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""method for MyInt class"""
+
+
+class MyInt(int):
+    """rebel version of int"""
+    def __new__(cls, *args, **kwargs):
+        """new instance of class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, param):
+        """inverts the functionality != """
+        return int(self) != param
+
+    def __ne__(self, param):
+        """inverts the functionality =="""
+        return int(self) == param
