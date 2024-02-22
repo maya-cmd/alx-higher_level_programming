e54ac5eaa15bebcbddaef830341301c60200893f
Sheelah <sheelah.mogaka@gmail.com>
2024-02-21 18:15:31 -0800

    101-stats.

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,15 +1,46 @@
 #!/usr/bin/python3
-import random
-import sys
-from time import sleep
-import datetime
+"""A script that reads stdin line by line and computes metrics:"""
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+
+from sys import stdin
+
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '402': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+
+i = 0
+total_size = i
+
+
+def print_metrics():
+    """Function prints the data"""
+    print(f"File size: {total_size}")
+    for key, value in sorted(status_codes.items()):
+        if value > 0:
+            print("{:s}: {:d}".format(key, value))
+
+
+try:
+    for line in stdin:
+        split_line = line.split()
+        if len(split_line) >= 2:
+            status = split_line[-2]
+            total_size += int(split_line[-1])
+            if status in status_codes:
+                status_codes[status] += 1
+
+        i += 1
+
+        if (i % 10) == 0:
+            print_metrics()
+    print_metrics()
+except KeyboardInterrupt as e:
+    print_metrics()
