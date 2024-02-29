00c8cf4b3cb81a0208e4f1f7d2edb737d12e452e
Sheelah <sheelah.mogaka@gmail.com>
2024-02-29 04:49:05 -0800

    To JSON

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,3 +20,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+     @staticmethod
+    def to_json_string(list_dictionaries):
+        """Method outputsthe JSON serialization of a dicts'list.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
