a8e32a21e1268665b8b9da2b4877f6c2c4c27eaa
Sheelah <sheelah.mogaka@gmail.com>
2024-03-27 18:41:56 -0700

    10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT title, genre_id FROM tv_shows
+FULL JOIN tv_show_genres ON id = show_id
+ORDER BY title ASC, genre_id ASC;
