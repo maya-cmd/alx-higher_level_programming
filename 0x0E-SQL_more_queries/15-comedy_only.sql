755b107f9964c78e7aaabb660b37f79d795233d2
Sheelah <sheelah.mogaka@gmail.com>
2024-03-27 19:13:13 -0700

    15-comedy_only.sql

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,5 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = show_id
+JOIN tv_genres ON genre_id = tv_genres.id WHERE `name` = "Comedy"
+ORDER BY title;
