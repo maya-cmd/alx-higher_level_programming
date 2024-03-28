279c9333258e180ccd328aaad5d474adba107a9d
Sheelah <sheelah.mogaka@gmail.com>
2024-03-27 19:02:41 -0700

    14-my_genres.sql

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+-- uses the hbtn_0d_tvshows database to list all genres of the show Dexter
+SELECT name FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = genre_id
+JOIN tv_shows ON tv_shows.id = show_id WHERE title = "Dexter"
+ORDER BY `name`;
