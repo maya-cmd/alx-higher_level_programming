365edda40020d002107d87c4907f50dd78befa9f
Sheelah <sheelah.mogaka@gmail.com>
2024-05-03 07:14:20 -0700

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Lists all cities from
+the database `hbtn_0e_4_usa`.
+"""
+
+import MySQLdb as db
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access the database and get the cities
+    from the database.
+    """
+
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+
+    with db_connect.cursor() as db_cursor:
+        db_cursor.execute("SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC")
+        all_rows = db_cursor.fetchall()
+
+    if all_rows is not None:
+        for row in all_rows:
+            print(row)
