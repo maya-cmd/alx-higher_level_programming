39b9be31da110b282b5788e862b7be06e8a5e991
Sheelah <sheelah.mogaka@gmail.com>
2024-05-03 08:39:28 -0700

    101-relationship_states_cities_list.py.

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
 
-    for state in states:
+    for state in st:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
