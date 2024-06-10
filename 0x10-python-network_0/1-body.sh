f3a237358639a813aa785cf95ab5fef3b5bf64a3
Sheelah <sheelah.mogaka@gmail.com>
2024-06-10 02:43:50 -0700

    1-body.sh updated

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 #takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -s -L -X GET "$1"
-<<<<<<< HEAD
-~
-=======
->>>>>>> 0e365cc7c8e4128b317a424a83958a8d1d9adb44
